523f798b184b6295f2d30d91c3b836a20c750828
Daniel <dani.b.g.castro@gmail.com>
2023-10-04 23:45:01 -0300

    FirstCommit
    Creation of a README file carrying some interesting informations about the project; Creation of the first layout...

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  console.log('App executando');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Welcome to WATER WISE</Text>
+
+      <View style={styles.input}>
+        <Button style={styles.buttonInput} title='Log in'></Button>
+        <Button style={styles.buttonInput} title='Sign in'></Button>
+      </View>
+
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -16,5 +25,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    display: 'inline',
   },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#437AF0',
+    backgroundColor: '#1F3970',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    flexDirection : 'row',
+  },
+
+  titleText: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: '#1F3970',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#437AF0',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    
+  },
+
+  buttonInput: {
+    borderWidth: 1,
+    borderColor: '#437AF0',
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+  }
 });
---
+++ README.md
@@ -0,0 +1,10 @@
+# Water Wise
+
+### Members of WounderWeb II: 
+* Carlos Frederico
+* João Vitor
+* Isabella 
+* Daniel Castro
+
+### The project idea:
+* An application that provides informative and simple interface about th waterway of the user. 
